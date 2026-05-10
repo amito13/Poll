580e36ac486e316413c2806cdf5cf94cc1ebca0a
AmitDewangan <dewanganamit40@gmail.com>
2026-05-10 10:17:27 +0530

    prisma => drizzle

---
+++ server/db/schema.ts
@@ -0,0 +1,17 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: text("id").primaryKey(),
+
+  name: text("name").notNull(),
+
+  email: text("email").notNull().unique(),
+
+  password: text("password").notNull(),
+
+  createdAt: timestamp("created_at").defaultNow(),
+});
