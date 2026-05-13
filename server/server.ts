57d22438416a270bd36cd92c057c4aa71d54b959
AmitDewangan <dewanganamit40@gmail.com>
2026-05-13 15:16:37 +0530

    cors update with vercel

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,10 @@ import cors from "cors";
 import pollRoutes from "./routes/poll.route.js";
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://poll-hazel.vercel.app/",
+}));
+
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
