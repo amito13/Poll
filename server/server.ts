2bfa782c9e73591cdbbc6162fa26331edff48361
AmitDewangan <dewanganamit40@gmail.com>
2026-05-15 03:52:20 +0530

    server fix from socket

--- server/server.ts
+++ server/server.ts
@@ -6,24 +6,24 @@ import http from "http";
 import { Server } from "socket.io";
 import pollRoutes from "./routes/poll.route.js";
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
-  },
-});
-import { setIo } from "./socket.js";
-setIo(io);
-io.on("connection", (socket) => {
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: process.env.FRONTEND_URL,
+//     methods: ["GET", "POST"],
+//   },
+// });
+//import { setIo } from "./socket.js";
+//setIo(io);
+//io.on("connection", (socket) => {
 
-  console.log("User connected:", socket.id);
+//   console.log("User connected:", socket.id);
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-  });
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected");
+//   });
 
-});
+// });
 app.use(cors({
   origin: process.env.FRONTEND_URL||"http://localhost:5173",
 }));
@@ -42,6 +42,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
